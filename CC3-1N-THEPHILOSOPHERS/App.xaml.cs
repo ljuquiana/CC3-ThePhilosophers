f048b9f3154a31c917c7cd651eb19d6de97497af
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-17 17:54:35 +0800

    Create App.xaml.cs

---
+++ CC3-1N-THEPHILOSOPHERS/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CC31N_ThePhilosophers;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
