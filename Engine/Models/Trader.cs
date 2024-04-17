a1fdfaa0f9a20258befe7ca8a56df135848f6c04
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-17 18:37:51 +0800

    Create Trader.cs

---
+++ Engine/Models/Trader.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Engine.Models
+{
+    public class Trader : LivingEntity
+    {
+        public Trader(string name) : base(name, 9999, 9999, 9999)
+        {
+            
+        }
+    }
+}﻿
