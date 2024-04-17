c4047a1f0593d55afa626e7c2d7dce4335491e90
ljuquiana <156904219+ljuquiana@users.noreply.github.com>
2024-04-17 18:38:28 +0800

    Create World.cs

---
+++ Engine/Models/World.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+namespace Engine.Models
+{
+    public class World
+    {
+        private readonly List<Location> _locations = new List<Location>();
+        internal void AddLocation(int xCoordinate, int yCoordinate,
+                                  string name, string description, string imageName)
+        {
+            _locations.Add(new Location(xCoordinate, yCoordinate, name, description,
+                                        $"/Engine;component/Images/Locations/{imageName}"));
+        }
+        public Location LocationAt(int xCoordinate, int yCoordinate)
+        {
+            foreach (Location loc in _locations)
+            {
+                if (loc.XCoordinate == xCoordinate && loc.YCoordinate == yCoordinate)
+                {
+                    return loc;
+                }
+            }
+            return null;
+        }
+    }
+}
